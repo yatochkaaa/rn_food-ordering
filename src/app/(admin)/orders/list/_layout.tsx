86c2f4d4ef29a883915ce05144f418b76efab727
Владимир Таранин <crm.c.30187@dz.com>
2024-09-03 17:30:52 +0300

    assigment update orders

---
+++ src/app/(admin)/orders/list/_layout.tsx
@@ -0,0 +1,17 @@
+import { withLayoutContext } from 'expo-router';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import OrdersScreen from '@/app/(admin)/orders/list';
+import Archive from '@/app/(admin)/orders/list/archive';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const TopTabs = withLayoutContext(createMaterialTopTabNavigator().Navigator);
+
+export default function OrderListNavigator() {
+  return (
+    <SafeAreaView edges={['top']} style={{ flex: 1, backgroundColor: 'white' }}>
+      <TopTabs>
+        <TopTabs.Screen name='index' options={{ title: 'Active' }}></TopTabs.Screen>
+      </TopTabs>
+    </SafeAreaView>
+  );
+}
